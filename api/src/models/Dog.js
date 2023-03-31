9a9b5ef466e26c8c9471e129ec1ffa0c85e81328
j10gof <juandiegoflorezlora@gmail.com>
2023-03-31 16:48:17 -0500

    Modelos dogs realizado

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,41 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    weightMin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weightMax: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    heightMin : {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    heightMax: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    life_span: {
+     type: DataTypes.STRING,
+     allowNull: false, 
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: false, }
+  );
 };
