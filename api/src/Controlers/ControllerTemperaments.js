a28df915cf28b7fcdd1fc0e6b082c3abf6ab5507
j10gof <juandiegoflorezlora@gmail.com>
2023-04-05 15:07:02 -0500

    Controllers Temperamentos Corregido

--- api/src/Controlers/ControllerTemperaments.js
+++ api/src/Controlers/ControllerTemperaments.js
@@ -3,11 +3,11 @@ const { Temperament } = require("../db.js");
 const { API_KEY } = process.env;
 
 const ApiToDbTemps = async function () {
-	let AllDataFromApi = await axios.get("`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`");
+	let AllDataFromApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`);
 	let tempsFromAPI = await AllDataFromApi.data.map((el) => ` ${el.temperament}`);
 	tempsFromAPI = tempsFromAPI.join().split(",").sort();
 	tempsFromAPI = [...new Set(tempsFromAPI)];
-	const formatTemperaments = tempsFromAPI.map((e) => e.trim()).filter((e) = e !== 'undefined');
+	const formatTemperaments = tempsFromAPI.map((e) => e.trim()).filter((e) => e !== "undefined");
 	//recorro la matriz de temperamentos formateados
 	for (let i = 0; i < formatTemperaments.length; i++) {
     const e = formatTemperaments[i];
