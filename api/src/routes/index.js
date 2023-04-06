d05f2670f19bed7dac1fd8c963d55b7070e57d9b
j10gof <juandiegoflorezlora@gmail.com>
2023-04-06 11:59:47 -0500

    Correccion ruta dog por ID

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const routerDog = require('./routerDog.js');
 const routerTemperaments = require('./routerTemperaments.js');
+const routerPostDog = require('./routerPostDog.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -11,5 +12,6 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/dogs", routerDog);
 router.use("/temperaments", routerTemperaments);
+router.use("/dog", routerPostDog);
 
 module.exports = router;
--- api/src/routes/routerDog.js
+++ api/src/routes/routerDog.js
@@ -25,7 +25,7 @@ router.get("/", async (req, res) => {
 	}
 });
 
-router.get(":id", async (req, res) => {
+router.get("/:id", async (req, res) => {
 	try {
 		const { id } = req.params;
 		const APIdogsInfo = await getAllInfo();
